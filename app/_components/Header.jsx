70de9b38eb7b5631bb2800648b48cb6797e552fb
Sandeep Avula <138490434+avula00@users.noreply.github.com>
2025-11-28 20:36:06 +0530

    Refactor Header component imports and formatting
    Updated import statements to use double quotes and adjusted JSX formatting.

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,19 +1,20 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-    <div className='flex justify-between p-5 shadow-sm'>
-      <Link href={'/'}>
-        <Image src={'/logo.svg'} width={150} height={100} alt='logo'/>
-        </Link>
-        <Link href={'/dashboard'}>
-          <Button>Get Started</Button>
-        </Link>
+    <div className="flex justify-between p-5 shadow-sm">
+      <Link href={"/"}>
+        <Image src={"/logo.svg"} width={150} height={100} alt="logo" />
+      </Link>
+
+      {/* <Button className="bg-blue-600 text-white hover:bg-blue-700">
+        About us
+      </Button> */}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
